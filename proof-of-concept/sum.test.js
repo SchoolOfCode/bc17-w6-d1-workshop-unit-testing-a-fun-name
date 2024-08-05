ef96ce1f1e37090a2f53cf4a153eee9be5329975
clw12 <clw6444@gmail.com>
2024-08-05 20:05:10 +0100

    Ticket 3

---
+++ proof-of-concept/sum.test.js
@@ -0,0 +1,17 @@
+//  import `test` and `expect` functions from Vitest
+//  import the `sum` function from sum.js
+//  create a bare bones test
+//  run Vitest to check that it detects and passes the new test
+//  inside the test:
+//      call the sum function with arguments: 5 and 7
+//      assert that the value returned is 12
+
+import { sum } from './sum.js';
+
+import { test, expect } from 'vitest'
+
+
+test('sumtest', () => {
+    expect(sum(5, 7)).toEqual(12);
+});
+
